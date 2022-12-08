7a29514f0a103549b4a1b9dac6ac050182d92ead
Rosana Veroneze <rveroneze@gmail.com>
2022-12-08 15:45:47 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -std=gnu++11 -g -Wall BicsUtils.cpp globalsv.cpp rinclosecvc.cpp rinclosecvcp.cpp stdafx.cpp RInClose.cpp -o RInClose_mminsup
+g++ -std=gnu++11 -g -Wall -Wextra BicsUtils.cpp globalsv.cpp rinclosecvc.cpp rinclosecvcp.cpp stdafx.cpp RInClose.cpp -o RInClose_mminsup
